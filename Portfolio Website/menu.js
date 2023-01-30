64cd94768440ad001f0941fc05443931acedc043
Olivia Ozbaki <76415978+viaoz@users.noreply.github.com>
2023-01-30 14:29:27 -0600

    Delete Portfolio Website directory

--- Portfolio Website/menu.js
+++
@@ -1,6 +0,0 @@
-const hMenu = document.querySelector('.hMenu');
-const offScreenMenu = document.querySelector('.off-screen-menu');
-hMenu.addEventListener('click', () => {
-	hMenu.classList.toggle('active');
-	offScreenMenu.classList.toggle('active');
-});
